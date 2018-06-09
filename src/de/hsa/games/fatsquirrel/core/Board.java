1a182332605eb051078f41f078e7fc8306bddf2f
Patrick Kostas <pa2kostas@yahoo.de>
2018-06-09 15:16:03 +0200

    Make Board use a List and add insert, delete etc... operations

--- src/de/hsa/games/fatsquirrel/core/Board.java
+++ src/de/hsa/games/fatsquirrel/core/Board.java
@@ -1,16 +1,26 @@
 package de.hsa.games.fatsquirrel.core;
 
-import de.hsa.games.fatsquirrel.entities.Entity;
+import de.hsa.games.fatsquirrel.entities.*;
+import de.hsa.games.fatsquirrel.util.XYSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
 
-    private Entity[][] entities;
+    private List<Entity> entities;
+    private int entityCounter;
     private BoardConfig boardConfig;
     private FlattenedBoard boardCache;
+    private int width, height;
 
-    public Board(BoardConfig config) {
-        this.boardConfig = config;
-        this.entities = new Entity[config.getSize().getX()][config.getSize().getY()];
+    public Board(BoardConfig boardConfig) {
+        this.boardConfig = boardConfig;
+        width = boardConfig.getSize().getX();
+        height = boardConfig.getSize().getY();
+        int boardSize = boardConfig.getSize().getX() * boardConfig.getSize().getY();
+        entities = new ArrayList<>(boardSize);
+        initBoard();
     }
 
     public BoardConfig getBoardConfig() {
@@ -19,6 +29,32 @@ public class Board {
 
     public XY getSize() {
         return boardConfig.getSize();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getEntityCounter() {
+        return entityCounter;
+    }
+
+    public void insertEntity(Entity entity) {
+        entities.add(entity);
+        entityCounter++;
+    }
+
+    public void deleteEntity(Entity entity) {
+        entities.remove(entity);
+        entityCounter--;
+    }
+
+    public List<Entity> getEntities() {
+        return entities;
     }
 
     public FlattenedBoard flatten() {
